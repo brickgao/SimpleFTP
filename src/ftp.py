f0c3b29d15ed97fe2e890766fced4aa281706a3c
Brickgao <brickgao@gmail.com>
2013-12-28 17:16:17 +0800

    fix func of LIST

--- src/ftp.py
+++ src/ftp.py
@@ -1,6 +1,6 @@
 # -*- coding:utf8 -*-
 
-import socket
+import socket, re
 
 class FTP():
 
@@ -8,6 +8,7 @@ class FTP():
         
         self.url = url
         self.loginSucc = False
+        self.currentList = []
 
     #If you want to login anonymous, just left account empty
     def login(self, account = 'anonymous', passwd = ''):
@@ -34,18 +35,41 @@ class FTP():
             __ = self.sock.recv(1024)
             if not '227' in __:     return False, __
             __ = __[27:-4].split(',')
-            self.h = __[:4]
-            self.p = __[4:]
+            self.h = __[0] + '.' + __[1] + '.' + __[2] + '.' + __[3]
+            self.p = int(__[4]) * 256 + int(__[5])
+            self.sockPasv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sockPasv.connect((self.h, self.p))
             self.loginSucc = True
             return True, _
 
-    def getCurrentList(self):
+    def retrlines(self, command):
 
-        self.sock.sendall('LIST\r\n')
-        _ = ''
-        while True:
-            print self.sock.recv(1024)
-        print _
+        #List Command: List the file in current directory
+        if command == 'LIST':
+            self.sock.sendall('LIST\r\n')
+            _ = self.sock.recv(1024)
+            if not '150' in _:   return False, _
+            _ = self.sockPasv.recv(8152)
+            _ = _.split('\r\n')[:-1]
+            self.currentList = []
+            for __ in _:
+                #Format List
+                ___ = re.findall(r'[\w|\-|>|/|\.]+', __)
+                ____ = {}
+                ____['permissions'] = ___[0]
+                ____['linkNum']     = ___[1] 
+                ____['owner']       = ___[2]
+                ____['ownerGroup']  = ___[3]
+                ____['date']        = ___[7] + ' ' + \
+                                      ___[6] + ' ' + \
+                                      ___[5]
+                ____['name']        = ___[6]
+                if ___[7] == '->':  ____['isLn'] = True
+                else:               ____['isLn'] = False
+                if ____['isLn']:    ____['Ln'] = ___[8]
+                self.currentList.append(____)
+            if '226' in _:       return True, _
+            
 
     def cwd(self, floder):
 
@@ -55,3 +79,5 @@ class FTP():
 if __name__ == '__main__':
     ftp = FTP('ftp.sjtu.edu.cn')
     _, recv = ftp.login()
+    ftp.retrlines('LIST')
+
